1ad437e64b817398d4981dc652adde13f84f24a3
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-02-23 02:03:55 -0800

    Add isEmpty method as a helper for the addFirst method

--- src/appHelperLinkedList/LinkedList1.java
+++ src/appHelperLinkedList/LinkedList1.java
@@ -19,4 +19,24 @@ public class LinkedList1 {
         }
     }
 
+    public void addFirst(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            node.setNext(first);
+            first = node;
+        }
+    }
+
+    public boolean isEmpty() {
+        boolean response = false;
+        if (first == null) {
+            response = true;
+        }
+        return response;
+    }
+
+
 }
