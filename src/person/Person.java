3caf9dce88f9f7168c0a08304c0ca0ac6c9646ec
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-02-23 01:44:03 -0800

    declaring the variable for the Person class

---
+++ src/person/Person.java
@@ -0,0 +1,11 @@
+package person;
+
+import java.util.LinkedList;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LinkedList<String> contacts;
+    private String email;
+
+}
