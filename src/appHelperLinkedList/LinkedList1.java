45c00c7c34752dbd0078448fc3dcaf5c006af87b
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-02-23 02:11:45 -0800

    add various method to display the desired resultsuch as display(), displayOne(), getNode()

--- src/appHelperLinkedList/LinkedList1.java
+++ src/appHelperLinkedList/LinkedList1.java
@@ -111,6 +111,55 @@ public class LinkedList1 {
         return index;
     }
 
+    public void display() {
+        var current = first;
+        System.out.println("Contact list--->");
+        System.out.println();
+        while (current != null) {
+            System.out.println("-----------------------*-------------------------");
+            System.out.println("Name:" + current.getValue().getFirstName() + " " + current.getValue().getLastName());
+            System.out.println("Contacts:" + current.getValue().getContacts().toString());
+            System.out.println("Email:" + current.getValue().getEmail());
+            System.out.println("------------------------*-------------------------");
+            System.out.println();
+            current = current.getNext();
+        }
+        System.out.println();
+    }
+
+    public void displayOne() {
+        var current = first;
+        int count = 0;
+        System.out.println("-----------------------*-------------------------");
+        while (current != null) {
+            System.out.println(count + " Name:" + current.getValue().getFirstName() + " " + current.getValue().getLastName());
+            current = current.getNext();
+            count++;
+        }
+        System.out.println("------------------------*-------------------------");
+        System.out.println();
+    }
+
+    public void getNode(int pos) {
+        var current = first;
+        int index = 0;
+        while (current != null) {
+            if (index == pos) {
+                System.out.println();
+                System.out.println("-----------------------*-------------------------");
+                System.out.println("Name:" + current.getValue().getFirstName() + " " + current.getValue().getLastName());
+                System.out.println("Contacts:" + current.getValue().getContacts().toString());
+                System.out.println("Email:" + current.getValue().getEmail());
+                System.out.println("------------------------*-------------------------");
+                System.out.println();
+                break;
+            }
+            index++;
+            current = current.getNext();
+        }
+    }
+
+
 
 
 }
